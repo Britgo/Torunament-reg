c6b9b933bdc069f3817e3a48d863510c685415a1
John M Collins <jmc@toad.me.uk>
2016-08-01 18:42:01 +0100

    Add missing files.

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>BGA Tournament Registration</title>
+<meta name="generator" content="Bluefish 2.2.5" >
+<meta name="author" content="John M Collins" >
+<meta name="date" content="2016-08-01T18:33:00+0100" >
+<meta name="copyright" content="">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="expires" content="0">
+<link href="tournreg.css" type="text/css" rel="stylesheet"></link>
+</head>
+<body>
+<?php include 'php/nav.php';
+include 'php/tcerror.php';
+include 'php/tdate.php';
+include 'php/person.php';
+include 'php/tournclass.php';
+include 'php/opendb.php';
+
+try  {
+	opendb();
+	$tlist = get_tcodes("sdate desc,tname", true, true);
+	if  (count($tlist) == 0)
+		print "<p>There are currently no tournaments to list.</p>\n";
+	else  {
+		print <<<EOT
+<table>
+<tr>
+	<th>Enter</th>
+	<th>List</th>
+	<th>Date(s)</th>
+	<th>Venue</th>
+	<th>Postcode</th>
+	<th>Format</th>
+	<th>Overview</th>
+	<th>ICS</th>
+</tr>
+EOT;
+
+		foreach ($tlist as $tc)  {
+			$tourn = new Tournament($tc);
+			$tourn->fetchdets();
+			$url = $tourn->urlof();
+			$codeprin = $tourn->display_code();
+			$nameprin = $tourn->display_name();
+			if  ($tourn->Open)  {
+				if  (!$tourn->is_over())
+					$codeprin = "<a href=\"http://www.britgo.org/tournaments/_register/form$url\">$codeprin</a>";
+				$nameprin = "<a href=\"http://www.britgo.org/tournaments/_register/list$url\">$nameprin</a>";
+			}
+			print <<<EOT
+<tr>
+	<td>$codeprin</td>
+	<td>$nameprin</td>
+	<td>{$tourn->display_dates()}</td>
+	<td>{$tourn->display_addr()}</td>
+	<td>{$tourn->display_pc()}</td>
+	<td>{$tourn->display_format()}</td>
+	<td>{$tourn->display_over()}</td>
+	<td><a href="http://www.britgo.org/tournreg/downloadics.php{$tourn->urlof()}">ICS</a></td>
+</tr>
+
+EOT;
+		}
+		print <<<EOT
+</table>
+EOT;
+	}
+}
+catch (Tcerror $e)  {
+	$hdr = $e->Header;
+	$msg = htmlspecialchars($e->getMessage());
+	print <<<EOT
+<h1>$hdr</h1>
+<p>$msg</p>
+
+EOT;
+	return;
+}
+?>
+</div>
+</div>
+</body>
+</html>
---
+++ php/nav.php
@@ -0,0 +1,37 @@
+<?php
+// Copyright John Collins 2016
+// Licensed under the GPL, v3
+
+// *****************************************************************************
+// PLEASE BE CAREFUL ABOUT EDITING THIS FILE, IT IS SOURCE-CONTROLLED BY GIT!!!!
+// Your changes may be lost or break things if you don't do it correctly!
+// *****************************************************************************
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+?>
+<div id="Nave">
+<div class="innertube">
+<table>
+<tr>
+	<td><a href="https://www.britgo.org" title="Go to BGA main site"><img src="images/gohead12.gif" width="133" height="47" alt="BGA Logo" border="0" hspace="0" vspace="0"></a></td>
+	<td><a href="index.php" title="Go to tournament registration home page">League Home</a><</td>
+	<td><a href="https://www.britgo.org/tournaments" title="View BGA tournament calendar">BGA Calendar<</a>/td>
+	<td><a href="https://www.britgo.org/results/12months" title="View last 12 months results">Results</a></td>
+</tr>
+</table>
+</div>
+</div>
+<div id="Content">
+<div class="innertube">
