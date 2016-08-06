c5b8766abd442ee0be7de3d2577673a0a94ec639
John M Collins <jmc@toad.me.uk>
2016-08-06 23:33:27 +0100

    Fix delete entry.

--- delentry.php
+++ delentry.php
@@ -1,39 +1,45 @@
 <?php
 
-// Copyright John Collins 2014
-// Licensed under the GPL, v3
+//   Copyright 2016 John Collins
 
 // *****************************************************************************
 // PLEASE BE CAREFUL ABOUT EDITING THIS FILE, IT IS SOURCE-CONTROLLED BY GIT!!!!
 // Your changes may be lost or break things if you don't do it correctly!
 // *****************************************************************************
 
-include 'tcerror.php';
-include 'tdate.php';
-include 'rank.php';
-include 'person.php';
-include 'entrant.php';
-include 'tournclass.php';
-include 'opendb.php';
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
 
-if (!preg_match(';/tournaments/_?register/listall;', $_SERVER["HTTP_REFERER"]))  {
-	print <<<EOT
-<h1>Wrong entry</h1>
-<p>This has not been invoked correctly, byebye.</p>
-<p>Referrer was {$_SERVER["HTTP_REFERER"]}</p>
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
 
-EOT;
-	sleep(109);
-	return;
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+include 'php/tcerror.php';
+include 'php/session.php';
+include 'php/checklogged.php';
+include 'php/tdate.php';
+include 'php/rank.php';
+include 'php/person.php';
+include 'php/entrant.php';
+include 'php/tournclass.php';
+include 'php/opendb.php';
+
+if (!$admin)  {
+	$mess = 'Not admin';
+	include 'php/wrongentry.php';
+	exit(0);
 }
 
 if (!isset($_GET['tcode'])  || !isset($_GET['f'])  ||  !isset($_GET['l']))  {
-print <<<EOT
-<h1>Wrong entry</h1>
-<p>I do not know how you got here, but it is wrong</p>
-
-EOT;
-	return;
+	$mess = 'Codes not set';
+	include 'php/wrongentry.php';
+	exit(0);
 }
 
 $tcode = $_GET['tcode'];
@@ -42,19 +48,29 @@ try  {
 	opendb();
 	$tourn = new Tournament($tcode);
 	$tourn->fetchdets();
-	$pers = new Entrant($_GET['f'], $_GET['l']);
+	$pers = new Entrant();
+	$pers->fromget();
 	$pers->del($tourn);
 }
 catch (Tcerror $e)  {
-	$hdr = $e->Header;
-	$msg = htmlspecialchars($e->getMessage());
-	print <<<EOT
-<h1>$hdr</h1>
-<p>$msg</p>
-
-EOT;
-	return;
+	$mess = 'Invalid codes';
+	include 'php/wrongentry.php';
+	exit(0);
 }
 
-header("Location: http://www.britgo.org/tournaments/_register/listall{$tourn->urlof()}");
+$Title = 'Entrant deleted';
+include 'php/head.php';
 ?>
+<body>
+<?php
+include 'php/nav.php';
+print <<<EOT
+<h1>Entrant deleted</h1>
+<p>The entrant {pers->display_name()} has been removed from {$tourn->display_name()}.</p>
+
+EOT;
+?>
+</div>
+</div>
+</body>
+</html>
