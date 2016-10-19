77b9981c9f3c2e54a5d18c414fee5ba67859d631
John M Collins <jmc@toad.me.uk>
2016-10-19 16:37:53 +0100

    Try to work round non-parsing JSes.

--- webfn.js
+++ webfn.js
@@ -27,8 +27,8 @@ function okcode(s)  {
 }
 
 function okname(s)  {
-	return /^[A-Z][a-z]*( +(Mc|O')?[A-Z][a-z]*(-[A-Z][a-z]*)?)+$/.test(s);
-	//'
+	//return /^[A-Z][a-z]*( +(Mc|O')?[A-Z][a-z]*(-[A-Z][a-z]*)?)+$/.test(s);
+	return /^[-_a-zA-Z]+ [-_a-zA-Z]+$/.test(s);
 }
 
 function lostpw() {
