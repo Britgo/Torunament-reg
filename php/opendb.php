64b110114ac2674a72fcbb82292fcee7d949d815
John M Collins <jmc@toad.me.uk>
2017-01-22 11:40:18 +0000

    Move credentials out of source code.

--- php/opendb.php
+++ php/opendb.php
@@ -1,18 +1,42 @@
 <?php
 
+//   Copyright 2014-2017 John Collins
+
+// *****************************************************************************
+// PLEASE BE CAREFUL ABOUT EDITING THIS FILE, IT IS SOURCE-CONTROLLED BY GIT!!!!
+// Your changes may be lost or break things if you don't do it correctly!
+// *****************************************************************************
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 // Open database and throw error if not nice.
+
+include 'credentials.php';
 
 function opendb()
 {
 
-	//  CHANGE THESE AS REQUIRED!!!!
-
-	$hostname = "localhost";	
-	$username = "tourn-reg";
-	$password = "tourn-reg-0601";
-	$dbname = "tournaments";
+	try  {
+		$dbcred = getcredentials('tournreg');
+	}
+	catch (Credentials_error $e)  {
+		$ecode = $e->getMessage();
+		throw new Tcerror("Cannot get DB credentials, error was $ecode", "DB credentials error");
+	}
 	
-	if  (!mysql_connect($hostname, $username, $password)  ||  !mysql_select_db($dbname))  {
+	if  (!mysql_connect("localhost", $dbcred->Username, $dbcred->Password)  ||  !mysql_select_db($dbcred->Databasename))  {
 		$ecode = mysql_error();
 		throw  new  Tcerror("Cannot open database, error was $ecode", "Database error");
 	}
